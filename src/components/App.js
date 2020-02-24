d9fc286eaf24d086f183aedbeb298fa15486f8de
Kyle <kyle.fetterhoff@gmail.com>
2020-02-24 14:57:00 -0500

    Add picture and import into app

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import "./styles/app.css";
 
+import img from "../assets/hello.jpg";
+
 class App extends Component {
   render() {
     return (
       <div className='App'>
         <h1>My React App</h1>
+        <img src={img} style={{ width: "200px", height: "200px" }} />
       </div>
     );
   }
